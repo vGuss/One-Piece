90555c536576481ecc4eb494e043cc2811634924
Gu_HeAl <gu.halmeida@hotmail.com>
2024-08-15 21:06:10 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+/* 
+  O que precisamos fazer? - quando clicar no botão do personagem na lista, temos que marcar o botão como selecionado e mostrar o personagem correspondente
+
+    OBJETIVO 1 - quando clicar no botão do personagem na lista, marcar o botao como selecionado 
+        passo 1 - pegar os botões no JS pra poder verificar quando o usuário clicar em cima de um deles
+        passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
+        passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele 
+
+        OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem
+        passo 1 - pegar os personagens no JS pra poder mostrar ou esconder ele
+        passo 2 - adicionar a classe "selecionado" no personagem que o usuário selecionou
+        passo 3 - verificar se já existe um personagem selecionado, se sim, devemos remover a seleção dele 
+*/
+
+//Objetivo 1
+
+//passo - 1
+const botoes = document.querySelectorAll(".botao")
+
+//Objetivo 2
+
+//passo - 1
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+        botao.addEventListener("click", () => {
+
+                // passo - 3
+                desselecionarBotao();
+
+                // passo - 2
+                botao.classList.add("selecionado");
+
+                // passo - 3
+                desselecionarPersonagem();
+
+                // passo - 2
+                personagens[indice].classList.add("selecionado");
+        });
+});
+function desselecionarPersonagem() {
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado");
+}
+
